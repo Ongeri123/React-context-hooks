803f7d7fd3dd2e316f68a36a18de01089e5ddb11
Ongeri123 <ongerinewton10@gmail.com>
2025-12-01 15:28:00 +0300

    built the context app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './index.css'
+import BookList from './Component/Book-list.jsx'
+import Navbar from './Component/Navbar.jsx'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Navbar />
+      <BookList/>
+    </div>
+  )
+}
+
+export default App
